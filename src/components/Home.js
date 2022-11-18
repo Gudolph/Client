4d161ec43f322bc18055697daf43443d247e177c
Chanran33 <k55416325@gmail.com>
2022-11-18 22:39:11 +0900

    route 설치

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>연말 캘린더</h1>
+      <button type="button">로그인/로그아웃</button>
+      <button type="button">시작하기</button>
+    </div>
+  );
+};
+
+export default Home;
